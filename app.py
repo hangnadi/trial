77ef6f269e2ffd920c512f737bce972b84ebdde4
Hang  Nadi <hangnadi@Hangs-MBP22.local>
2025-07-27 18:33:40 +0700

    add logic sum positive

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ def even_or_odd(n: int) -> str:
     else: return "odd"
     # best answer
     # return "Even" if n % 2 == 0 else "Odd"
+
+def sum_positive(numbers: list[int]) -> int:
+    # your code here
+    i = 0
+    result = 0
+    while i < len(numbers):
+        if (numbers[i]) >= 0 :
+            result = result + numbers[i]
+        i=i+1
+    return result
+    # best answer
+    # return sum(n for n in numbers if n >= 0)
