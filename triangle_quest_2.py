14a115c9b3e629d0fd941a1d7e2d65f145032bae
Hang  Nadi <hangnadi@Hangs-MBP22.local>
2025-08-01 05:24:40 +0700

    add hackerrank : triangle_quest_2

--- triangle_quest_2.py
+++ triangle_quest_2.py
@@ -3,10 +3,10 @@ for i in range(
 ):  # More than 2 lines will result in 0 score. Do not leave a blank line also
     print(((10**i - 1) // 9) ** 2)
 
-    10 pangkat 2 = 100 - 1 = 99 / 9 = 11 pangkat 2 = 121
-    10 pangkat 3 = 1000 - 1 = 999 / 9 = 111 pangkat 2 = 12321
-    .
-    .
-    .
-    .
-    10 pangkat 5 = 
+    # 10 pangkat 2 = 100 - 1 = 99 / 9 = 11 pangkat 2 = 121
+    # 10 pangkat 3 = 1000 - 1 = 999 / 9 = 111 pangkat 2 = 12321
+    # .
+    # .
+    # .
+    # .
+    # 10 pangkat 5 = 100000 - 1 = 9999 / 9 = 1111 pangkat 2 = 123454321
