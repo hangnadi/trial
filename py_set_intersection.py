488ba523f1ea50f5656f61597d5d26dceaf873b2
Hang  Nadi <hangnadi@Hangs-MBP22.local>
2025-08-04 12:39:29 +0700

    add hackerrank py set intersection

---
+++ py_set_intersection.py
@@ -0,0 +1,10 @@
+
+A = int(input()) # the number of students who have subscribed to the English newspaper.
+list_A = input().rstrip().split() # ids of those students.
+B = int(input()) # the number of students who have subscribed to the French newspaper.
+list_B = input().rstrip().split() # ids of those students.
+
+# Find union
+print(len(set(list_A).intersection(set(list_B))))
+
+
